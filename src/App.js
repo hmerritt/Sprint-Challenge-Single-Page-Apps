c78378b493f39895443ed75ece0cab651f076600
Harry Merritt <harry@merritt.es>
2020-01-27 01:21:52 +0000

    Set-up react router to hook up the welcome and characters page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import * as Bootstrap from 'reactstrap';
 import Header from "./components/Header";
+import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
 
 
@@ -9,7 +11,10 @@ export default function App()
     return (
         <main>
             <Header />
-            <CharacterList />
+
+            {/* Routes */}
+            <Route path='/' exact component={WelcomePage} />
+            <Route path='/characters' component={CharacterList} />
         </main>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-    </header>
-  );
+    return (
+        <header className='ui centered'>
+            <h1 className='ui center'>Rick &amp; Morty Fan Page</h1>
+            <div>
+                <Nav>
+                    <NavItem>
+                        <Link to='/'>
+                            <NavLink>Home</NavLink>
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to='/characters'>
+                            <NavLink>Characters</NavLink>
+                        </Link>
+                    </NavItem>
+                </Nav>
+            </div>
+        </header>
+    );
 }
